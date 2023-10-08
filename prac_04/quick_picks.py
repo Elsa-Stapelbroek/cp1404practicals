830aa5db108eea4aeb55c17ec239a7406c9a634c
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-10-08 21:33:42 +1000

    Restructure quick-picks program

--- prac_04/quick_picks.py
+++ prac_04/quick_picks.py
@@ -1,11 +1,6 @@
-"""CP1404/CP5632 Practical 4 - Do-from-scratch exercise
-MAIN:
-ask input
-generate lines
-print
-"""
-import random
+"""CP1404/CP5632 Practical 4 - Do-from-scratch exercise"""
 
+from random import randint
 
 NUMBERS_IN_LINE = 6
 MINIMUM = 1
@@ -13,19 +8,19 @@ MAXIMUM = 45
 
 
 def main():
+    """Generate specified number of quick-picks and print neatly."""
     number_of_quick_picks = int(input("How many quick picks? "))
+    length_of_maximum = len(str(MAXIMUM))
+
     for i in range(number_of_quick_picks):
-        quick_pick = generate_numbers()
-
-
-def generate_numbers():
-    """Generate sequence containing specified amount of non-repeating numbers."""
-    numbers = []
-    while len(numbers) < NUMBERS_IN_LINE:
-        number = random.randint(MINIMUM, MAXIMUM)
-        if number not in numbers:
-            numbers.append(number)
-    return numbers
+        quick_pick = []
+        for j in range(NUMBERS_IN_LINE):
+            number = randint(MINIMUM, MAXIMUM)
+            while number in quick_pick:
+                number = randint(MINIMUM, MAXIMUM)
+            quick_pick.append(number)
+        quick_pick.sort()
+        print(' '.join(f"{number:{length_of_maximum}}" for number in quick_pick))
 
 
 main()
