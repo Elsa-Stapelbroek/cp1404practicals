ae55784250fe7bd7892701ce6bcd48b24815e05c
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-10-30 13:25:20 +1000

    Test Guitar class

---
+++ prac_06/guitar_test.py
@@ -0,0 +1,17 @@
+"""Guitars part 2: Testing"""
+from guitar import Guitar
+
+CURRENT_YEAR = 2023  # Doesn't affect program, maybe edit class for CURRENT_YEAR to be determined in client code?
+
+gibson = Guitar("Gibson L-5 CES", 1922, 16035.40)
+other = Guitar("another guitar", 2013)
+
+print(f"{gibson.name} get_age() - Expected {101}. Got {gibson.get_age()}")
+print(f"{other.name} get_age() - Expected {10}. Got {other.get_age()}")
+
+print(f"{gibson.name} is_vintage() - Expected {True}. Got {gibson.is_vintage()}")
+print(f"{other.name} is_vintage() - Expected {False}. Got {other.is_vintage()}")
+
+print(gibson)
+print(other)
+
