69b50c8094541420e4c5036d2246b3d1a3305373
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-11-02 20:21:50 +1000

    Adjust guitar program to get guitars from input and save the updated list to csv

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -2,7 +2,7 @@
 CP1404 practical 7 - More guitars!
 
 Estimate: 45 minutes
-Actual:
+Actual: ~50 minutes
 
 Format: Name,Year,Cost
 - Write a program to read all of these guitars and store them in a list of Guitar objects, using class.
@@ -12,8 +12,15 @@ Format: Name,Year,Cost
     - define how the < operator should work.
     - Write code for the __lt__ (less than) method so that it compares Guitars by year.
     - Then test and see if it sorts correctly now.
+
+- Add to your program to ask the user to enter their new guitars (just like your program from an earlier prac).
+    - Store these new guitars in your list of guitar objects, then
+    - write all of your guitars to the data file guitars.csv.
+- Test that this worked by opening the data file, and by running the program again to make sure it reads new guitars.
 """
 from prac_07.guitar import Guitar
+
+FILENAME = "guitars.csv"
 
 
 def main():
@@ -22,16 +29,37 @@ def main():
     guitars.sort()
     for guitar in guitars:
         print(guitar)
+    get_more_guitars(guitars)
+    save_guitars(guitars)
 
 
 def load_guitars():
     """Store guitar data from csv as list of Guitar objects."""
     guitars = []
-    with open("guitars.csv", 'r') as in_file:
+    with open(FILENAME, 'r') as in_file:
         for line in in_file:
             parts = line.strip().split(',')
             guitars.append(Guitar(parts[0], int(parts[1]), float(parts[2])))
     return guitars
+
+
+def get_more_guitars(guitars):
+    """Add guitars from user input to list until given an empty name."""
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: $"))
+        guitar = Guitar(name, year, cost)
+        guitars.append(guitar)
+        print(f"{guitar} added.")
+        name = input("Name: ")
+
+
+def save_guitars(guitars):
+    """Save updated guitar list to csv."""
+    with open(FILENAME, 'w') as out_file:
+        for guitar in guitars:
+            print(guitar.name, guitar.year, guitar.cost, sep=',', file=out_file)
 
 
 main()
