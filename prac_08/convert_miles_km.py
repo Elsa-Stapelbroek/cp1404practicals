ed5b716da7be9ecc398ceea0afd18a721d810e35
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-11-10 14:05:14 +1000

    Handle conversion Button press (miles to km)

--- prac_08/convert_miles_km.py
+++ prac_08/convert_miles_km.py
@@ -1,15 +1,27 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.properties import StringProperty
+
+CONVERSION_FACTOR = 1.609344
 
 
 class ConvertMilesKm(App):
     """"""
+    result = StringProperty()
 
     def build(self):
         """ build the Kivy app from the kv file """
         self.title = "Convert Miles to Kilometres"
         self.root = Builder.load_file('convert_miles_km.kv')
+        self.result = ''
         return self.root
+
+    def handle_convert(self):
+        """"""
+        try:
+            self.result = str(float(self.root.ids.input_number.text) * CONVERSION_FACTOR)
+        except ValueError:
+            pass
 
 
 ConvertMilesKm().run()
