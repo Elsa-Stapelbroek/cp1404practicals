d13050fb6ae970b2ae5b79455cd9a6566032daf9
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-11-15 13:22:16 +1000

    Add method docstrings to greeting app (prac8, boxlayout demo)

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,15 +4,19 @@ from kivy.lang import Builder
 
 
 class BoxLayoutDemo(App):
+    """Kivy App for greeting user based on text input."""
     def build(self):
+        """Build the Kivy GUI."""
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """Handle greet button press, display greeting with name from text input."""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """Handle clear button press, clear text input and greeting."""
         self.root.ids.input_name.text = ''
         self.root.ids.output_label.text = ''
 
