3f1dc0006115109884c61ddd2b7ec98331d9cb4d
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-11-15 16:35:15 +1000

    Complete prac 9 taxi example

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -1,7 +1,7 @@
 """CP1404 practical 9 - Walkthrough example: Test Taxi"""
 from prac_09.taxi import Taxi
 
-my_taxi = Taxi("Prius1", 100, 1.23)
+my_taxi = Taxi("Prius1", 100)
 my_taxi.drive(40)
 print(my_taxi)
 
