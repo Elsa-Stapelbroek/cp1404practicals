44af2d64b91f9652af29ab179e74035a5552a3f1
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-11-19 18:01:54 +1000

    Display current fare in taxi tests (prac 9)

--- prac_09/taxi_test.py
+++ prac_09/taxi_test.py
@@ -4,7 +4,9 @@ from prac_09.taxi import Taxi
 my_taxi = Taxi("Prius1", 100)
 my_taxi.drive(40)
 print(my_taxi)
+print(my_taxi.get_fare())
 
 my_taxi.start_fare()
 my_taxi.drive(100)
 print(my_taxi)
+print(my_taxi.get_fare())
