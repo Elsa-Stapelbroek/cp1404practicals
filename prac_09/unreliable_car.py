ae54569cfc078813383efb50afc3bd3496d6e882
Elsa-Stapelbroek <elsa.stapelbroek@my.jcu.edu.au>
2023-11-19 14:37:34 +1000

    Modify Unreliable Car to generate float instead of int and add tests

--- prac_09/unreliable_car.py
+++ prac_09/unreliable_car.py
@@ -3,17 +3,13 @@ CP1404 practical 9 - Intermediate exercise 1: Unreliable Car, class
 
 estimate: 35 minutes
 actual: 30 minutes
-- make subclass
-- add attr reliability: float between 0 and 100 (currently don't have anything ensuring the number is in that range)
-
-- fix class docstring!
 """
+from random import uniform
 from prac_09.car import Car
-from random import randint  # maybe it should be a float instead?
 
 
 class UnreliableCar(Car):
-    """Specialised version of Car that includes reliability ..."""
+    """Specialised version of Car that includes reliability percentage."""
     def __init__(self, reliability: float, **kwargs):
         """Initialise an Unreliable Car instance, based on parent class Car."""
         super().__init__(**kwargs)
@@ -21,7 +17,7 @@ class UnreliableCar(Car):
 
     def drive(self, distance):
         """Drive like parent Car if the generated number is lower than the reliability."""
-        if randint(0, 100) < self.reliability:
+        if uniform(0, 100) < self.reliability:
             distance_driven = super().drive(distance)
         else:
             distance_driven = 0
